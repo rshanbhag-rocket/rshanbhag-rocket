46b706a3a707dba9e8170e1cd2b8eba0191c7cd2
rshanbhag <rshanbhag@rocketsoftware.com>
2023-09-06 17:47:58 +0530

    ORMF-2169

--- docs/oxygen-webhelp/app/config.js
+++ docs/oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20230906124524';
+                args = '20230906174657';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;

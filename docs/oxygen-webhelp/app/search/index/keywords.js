376ad93350c94ccbd528a7cbf51d8ec4c14da877
rshanbhag <rshanbhag@rocketsoftware.com>
2023-09-08 21:15:46 +0530

    Grafana access via zosmf

--- docs/oxygen-webhelp/app/search/index/keywords.js
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -1,16 +1,19 @@
-define(function() {var keywords=[{w:"Applying",p:["p0","p1"]},{w:"transformation",p:["p0"]},{w:"to",p:["p0","p1"]},{w:"data",p:["p0","p1","p3"]},{w:"visualization",p:["p1","p10"]},{w:"Creating",p:["p2","p3"]},{w:"dashboards",p:["p2","p6"]},{w:"RMF",p:["p3","p8","p10"]},{w:"sources",p:["p3"]},{w:"Context",p:["p4"]},{w:"Sensitive",p:["p4"]},{w:"Help",p:["p4"]},{w:"Installing",p:["p5"]},{w:"the",p:["p5"]},{w:"plugin",p:["p5","p8"]},{w:"on",p:["p5"]},{w:"Grafana",p:["p5","p8","p10"]},{w:"Managing",p:["p6"]},{w:"Query",p:["p7"]},{w:"languages",p:["p7"]},{w:"IBM",p:["p8"]},{w:"for",p:["p8"]},{w:"z/OS",p:["p8"]},{w:"App",p:["p8"]},{w:"Release",p:["p9"]},{w:"notes",p:["p9"]},{w:"of",p:["p10"]},{w:"Monitor",p:["p10"]},{w:"III",p:["p10"]},{w:"metrics",p:["p10"]},{w:"in",p:["p10"]}];
+define(function() {var keywords=[{w:"Accessing",p:["p0"]},{w:"the",p:["p0","p6","p8"]},{w:"Grafana",p:["p0","p6","p7","p8","p11","p13"]},{w:"dashboard",p:["p0"]},{w:"Applying",p:["p1","p2"]},{w:"transformation",p:["p1"]},{w:"to",p:["p1","p2"]},{w:"data",p:["p1","p2","p4"]},{w:"visualization",p:["p2","p13"]},{w:"Creating",p:["p3","p4"]},{w:"dashboards",p:["p3","p9"]},{w:"RMF",p:["p4","p11","p13"]},{w:"sources",p:["p4"]},{w:"Context",p:["p5"]},{w:"Sensitive",p:["p5"]},{w:"Help",p:["p5"]},{w:"Defining",p:["p6"]},{w:"server",p:["p6"]},{w:"through",p:["p7"]},{w:"IBM",p:["p7","p11"]},{w:"z/OS",p:["p7","p11"]},{w:"Management",p:["p7"]},{w:"Facility",p:["p7"]},{w:"Installing",p:["p8"]},{w:"plugin",p:["p8","p11"]},{w:"on",p:["p8"]},{w:"Managing",p:["p9"]},{w:"Query",p:["p10"]},{w:"languages",p:["p10"]},{w:"for",p:["p11"]},{w:"App",p:["p11"]},{w:"Release",p:["p12"]},{w:"notes",p:["p12"]},{w:"of",p:["p13"]},{w:"Monitor",p:["p13"]},{w:"III",p:["p13"]},{w:"metrics",p:["p13"]},{w:"in",p:["p13"]}];
 var ph={};
 ph["p0"]=[0, 1, 2, 3];
-ph["p1"]=[0, 4, 2, 3];
-ph["p2"]=[5, 6];
-ph["p3"]=[5, 7, 3, 8];
-ph["p4"]=[9, 10, 11];
-ph["p5"]=[12, 13, 14, 15, 16];
-ph["p6"]=[17, 6];
-ph["p7"]=[18, 19];
-ph["p8"]=[20, 7, 21, 22, 16, 23, 14];
-ph["p9"]=[24, 25];
-ph["p10"]=[4, 26, 7, 27, 28, 29, 30, 16];
+ph["p1"]=[4, 5, 6, 7];
+ph["p2"]=[4, 8, 6, 7];
+ph["p3"]=[9, 10];
+ph["p4"]=[9, 11, 7, 12];
+ph["p5"]=[13, 14, 15];
+ph["p6"]=[16, 1, 2, 17];
+ph["p7"]=[2, 18, 19, 20, 21, 22];
+ph["p8"]=[23, 1, 24, 25, 2];
+ph["p9"]=[26, 10];
+ph["p10"]=[27, 28];
+ph["p12"]=[31, 32];
+ph["p11"]=[19, 11, 29, 20, 2, 30, 24];
+ph["p13"]=[8, 33, 11, 34, 35, 36, 37, 2];
      return {
          keywords: keywords,
          ph: ph
--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {9:-1,1:-1,2:-1,6:-1,0:-1,3:-1,5:-1};
+return {1:-1,8:-1,12:-1,4:-1,0:7,9:-1,2:-1,7:-1,3:-1,6:7};
 });

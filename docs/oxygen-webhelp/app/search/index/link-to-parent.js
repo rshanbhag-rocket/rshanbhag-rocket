6c02dc316c1b44fcda32a2e3d11f2a61e847a21d
rshanbhag <rshanbhag@rocketsoftware.com>
2023-09-12 18:47:35 +0530

    Remove test output

--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++
@@ -1,4 +0,0 @@
-/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-define(function () {
-return {0:10,5:-1,4:1,15:-1,7:1,6:-1,1:-1,11:-1,10:-1,3:-1,12:-1,2:-1,14:-1,9:10,16:-1,17:-1,13:-1};
-});
